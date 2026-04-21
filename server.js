d24164ac4a17ac688b812323ca5c841512860ee1
LocalRepo <riteshshende616@gmail.com>
2026-04-21 11:36:21 +0530

    Frirst Commit

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const dns = require("dns");
+
+dns.setServers([
+  '8.8.8.8',
+  '8.8.4.4'
+])
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+/* ================= DB CONNECTION ================= */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ DB Error:", err.message));
+
+/* ================= USER MODEL ================= */
+const User = require("./models/User");
+
+/* ================= SIGNUP API ================= */
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists ❌" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful ✅" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error ❌" });
+  }
+});
+
+/* ================= LOGIN API ================= */
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found ❌" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password ❌" });
+    }
+
+    res.json({
+      message: "Login successful ✅",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error ❌" });
+  }
+});
+
+/* ================= SERVER ================= */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on ${PORT}`);
+});
